84d934545e7a0065c7eb848daf6bfa65d5315841
Jonas Ohlsson <jonas@vansinne.se>
2025-03-21 09:14:58 +0100

    add support for older kraken2 format

--- gracken/data_loaders.py
+++ gracken/data_loaders.py
@@ -15,9 +15,30 @@ def read_bracken_report(file_path):
 def read_kraken2_report(file_path):
     """Extract species abundances from Kraken2 report."""
     df = pd.read_csv(file_path, sep="\t", header=None)
-    filtered = df[(df[5] == "S") & (df[1] > 0)]
+
+    # Determine the format based on the number of columns
+    num_cols = df.shape[1]
+
+    # Different KRaken2 formats:
+    # XXX: there might be further formats
+    # %coverage, #reads, #reads direct, rank code, NCBI ID, name
+    if num_cols == 6:
+        rank_col = 3
+        taxid_col = 4
+        name_col = 5
+    # %coverage, #reads, #reads direct, ?, ?, rank code, NCBI ID, name
+    elif num_cols == 8:
+        rank_col = 5
+        taxid_col = 6
+        name_col = 7
+    else:
+        raise ValueError(f"Unsupported Kraken2 report format: {num_cols} columns")
+
+    filtered = df[(df[rank_col] == "S") & (df[1] > 0)]
+
     result = pd.DataFrame()
-    result["abundance"] = filtered[3]
-    result["taxid"] = filtered[6] if df.shape[1] > 6 else ""
-    result["species"] = filtered[7].str.strip() if df.shape[1] > 7 else ""
+    result["abundance"] = filtered[1]
+    result["taxid"] = filtered[taxid_col]
+    result["species"] = filtered[name_col].str.strip()
+
     return result
